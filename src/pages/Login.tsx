b9bfbe1427bd12302d486a60d9d79cf05ceffa7e
dwaynediddy <burrows.dwayne@icloud.com>
2022-03-23 22:25:06 +0100

    small changes for phone view

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import '../login.css'
 
 const Login = () => {
-
-    const handleLogin = (e:React.FormEvent<HTMLButtonElement>) => {
+    const handleLogin = (e: React.FormEvent<HTMLFormElement>): void => {
         e.preventDefault()
+        const { name, password } = e.target as typeof e.target & {
+            name: {value: string}
+            password: {value: string}
+        }
+        console.log(name.value, password.value)
     }
 
   return (
     <div className="loginContainer">
         <h3>Login</h3>
-        <form className='formContainer'>
+        <form className='formContainer'
+            onSubmit={handleLogin}
+        >
             <label className='loginLabel'>Enter Teams Name</label>
             <input 
                 type='text'
@@ -21,15 +27,15 @@ const Login = () => {
                 />
             <label className='loginLabel'>Enter Teams Password</label>
             <input 
-                type='text'
+                type='password'
                 name='password'
                 placeholder='Enter Password'
                 className="loginInput"
             />
         </form>
-            <Link to='/bingo'>
-                <button className='loginButton' onSubmit={handleLogin}>Login</button>
-            </Link>
+            {/* <Link to='/bingo'> */}
+                <button className='loginButton' type='submit'>Login</button>
+            {/* </Link> */}
     </div>
     
   )
