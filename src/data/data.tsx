a65778de5a4ed78ca9d5606a340ea5af3d7623ca
dwaynediddy <burrows.dwayne@icloud.com>
2022-03-25 16:58:06 +0100

    moved data abandon changing style with state

---
+++ src/data/data.tsx
@@ -0,0 +1,20 @@
+export const lineOne = [
+    {id: 1, title: "one", img: 'avatar'},
+    {id: 2,title: "two", img: "claws"},
+    {id: 3,title: "three", img: "avatar"},
+    {id: 4,title: "four", img: "claws"},
+]
+
+export const lineTwo = [
+    {id: 5, title: "this is working", img: 'claws'},
+    {id: 6, title: "two", img: "avatar"},
+    {id: 7, title: "three", img: "claws"},
+    {id: 8, title: "four", img: "avatar"},
+]
+
+export const lineThree = [
+    {id: 9, title: "this is working", img: 'avatar'},
+    {id: 10, title: "two", img: "claws"},
+    {id: 11, title: "three", img: "claws"},
+    {id: 12, title: "four", img: "avatar"},
+]
--- src/pages/Bingo.tsx
+++ src/pages/Bingo.tsx
@@ -1,35 +1,12 @@
 import { useState } from 'react'
 import '../bingo.css'
-
-const lineOne = [
-    {id: 1, title: "one", img: 'avatar'},
-    {id: 2,title: "two", img: "claws"},
-    {id: 3,title: "three", img: "avatar"},
-    {id: 4,title: "four", img: "claws"},
-]
-
-const lineTwo = [
-    {id: 5, title: "this is working", img: 'claws'},
-    {id: 6, title: "two", img: "avatar"},
-    {id: 7, title: "three", img: "claws"},
-    {id: 8, title: "four", img: "avatar"},
-]
-
-const lineThree = [
-    {id: 9, title: "this is working", img: 'avatar'},
-    {id: 10, title: "two", img: "claws"},
-    {id: 11, title: "three", img: "claws"},
-    {id: 12, title: "four", img: "avatar"},
-]
-
+import {lineOne, lineTwo, lineThree} from '../data/data'
 
 const Bingo = () => {
-    const [clicked, setClicked] = useState('unSelected')
+    const [clicked, setClicked] = useState()
 
     const squareComplete = () => {
-        console.log('change style')
 
-        setClicked("selected")
     }
 
   return (
@@ -37,37 +14,16 @@ const Bingo = () => {
         <h3>Bingo</h3>
         <div className='rowOne'>
         {lineOne.map((square, index) => (
-            <div key={index}>
-                <button className={clicked} onClick={squareComplete}>
+            <tr key={index}>
+                <button className='bingoBtn' onClick={squareComplete}>
                     <img 
                         src={require(`../images/${square.img}.png`)}
                         alt={square.title}
-                        className={clicked}
                     />
                 </button>
-            </div>
+            </tr>
         ))}
         </div>
-        {/* <div className='rowTwo'>
-        {lineTwo[0].rowOne.map((square, index) => (
-            <div key={index} className="bingoSquares">
-                <img 
-                     src={require(`../images/${square.img}.png`)}
-                     alt={square.title}
-                />
-            </div>
-        ))}
-        </div>
-        <div className='rowThree'>
-        {lineThree[0].rowOne.map((square, index) => (
-            <div key={index} className="bingoSquares">
-                <img 
-                     src={require(`../images/${square.img}.png`)}
-                     alt={square.title}
-                />
-            </div>
-        ))}
-        </div> */}
     </div>
   )
 }
